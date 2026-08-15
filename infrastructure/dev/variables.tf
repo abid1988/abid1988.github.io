701e789c2ae86b0e3bc34e1f5e81e7cf0c542d00
abid1988 <abid.javi@gmail.com>
2026-08-15 15:22:45 +0530

    infra: add terraform dev configuration

---
+++ infrastructure/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
